241c93b744ca6643eb5df38a1780ebe217a7f654
Steve Kennedy <SteveKennedy2019@outlook.com>
2020-01-22 22:54:12 -0600

    Requirement 4 - Added Unlimted Preference Overrides to Person

---
+++ NelnetProgrammingExercise/Models/PreferenceOverride.cs
@@ -0,0 +1,9 @@
+﻿namespace NelnetProgrammingExercise.Models
+{
+    public class PreferenceOverride
+    {
+        public PetClassification PreferredClassification { get; set; }
+        public PetType PreferredType { get; set; }
+        public PetWeightCategory PreferredWeightCategory { get; set; }
+    }
+}
